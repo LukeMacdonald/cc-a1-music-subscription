3fd142adae2bcab9b00f300709b0b032284baaaa
Luke <lukemacdonald21@gmail.com>
2023-03-30 16:15:06 +1100

    Implemented more

--- app.py
+++ app.py
@@ -1,130 +1,154 @@
-import base64
-
+import os
 from flask import Flask, session, render_template, request, redirect, url_for
 import json
 import boto3
 import requests
 
 app = Flask(__name__)
-app.secret_key = "SECRETKEY"
 
 lambda_client = boto3.client('lambda', region_name='us-east-1')
-api = boto3.client('apigateway', region_name='us-east-1')
-api_url = "https://ikv7ph4k04.execute-api.us-east-1.amazonaws.com/default/ccA1Lambda"
-s3 = boto3.client('s3')
-bucket_name = 'myfirstbucket-luke'
-key = 'ArcadeFire.jpg'
-url = 'https://et2jb8ggg8.execute-api.us-east-1.amazonaws.com/dev'
+url = os.environ.get('API_URL')
+app.secret_key = os.environ.get('SESSION_KEY')
 
 
 @app.route('/', methods=['GET'])
-def login():
+def landing():
     if 'username' in session:
         return load_home()
-    # response = s3.get_object(Bucket=bucket_name, Key=key)
-    # image_data = response['Body'].read()
-    # encoded_image = base64.b64encode(image_data).decode('utf-8')
     return render_template("login.html")
 
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('login'))
-
-
-@app.route('/findsongs', methods=['POST'])
-def find_songs():
-    data = {
-        'artist': request.form.get('artist'),
-        'title': request.form.get('title'),
-        'year': request.form.get('year')
-    }
-    print(data)
-
-    response = requests.get(
-        url=url + '/music',
-        headers={'Content-Type': 'application/json'},
-        params=data
-    )
-    response_body = response.json()
-
-    return render_template("subscription.html",songs=response_body['Items'])
-
-
-@app.route('/addsubscription')
-def add_subscription():
-    # data = {
-    #     'username': request.form.get('user_name'),
-    #     'artist': request.form.get('artist'),
-    #     'title': request.form.get('title'),
-    #     'path': request.form.get('path'),
-    #     'year': request.form.get('year')
-    # }
-    #
-    # response = requests.post(
-    #     url=url + '/subscription',
-    #     headers={'Content-Type': 'application/json'},
-    #     data=json.dumps(data)
-    # )
-
-    return render_template("subscription.html")
-
-
-@app.route('/validate', methods=['POST'])
-def validate():
+@app.route('/login', methods=['POST'])
+def login():
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         if not email or not password:
             raise Exception('Email and Password Required')
 
-        response = requests.get(
-            url=url + '/login',
-            headers={'Content-Type': 'application/json'},
-            params={'email': email}
-        )
-        response_body = response.json()
-        if response.status_code == 200:
-            if response_body['password'] != password:
-                raise Exception('Incorrect Password')
-            else:
-                session['username'] = response_body['user_name']
-                return load_home()
+        response = get_request('/login', {'email': email, 'password': password})
+
+        if response['status_code'] == 200:
+
+            session['username'] = response['body']['user_name']
+            return load_home()
         else:
-            raise Exception('Email Not Found')
+            raise Exception(response['body'])
 
     except Exception as e:
         error = str(e)
         return render_template('login.html', error_message=error)
 
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('landing'))
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = {
+        'username': request.form.get('username'),
+        'email': request.form.get('email'),
+        'password': request.form.get('password')
+    }
+
+    response = post_request('/login', data)
+
+    if response['status_code'] == 200:
+        session['username'] = request.form.get('username')
+        return load_home()
+    else:
+        return render_template('register.html', error_message=response['body'])
+
+
+@app.route('/songs/search', methods=['POST', 'GET'])
+def find_songs():
+    data = {
+        'artist': request.form.get('artist'),
+        'title': request.form.get('title'),
+        'year': request.form.get('year')
+    }
+
+    response = get_request('/music/query', data)['body']
+    return render_template("query.html", songs=response)
+
+
+@app.route('/songs', methods=['GET'])
+def search():
+    return render_template("subscription.html", songs=[])
+
+
+@app.route('/subscription', methods=['POST'])
+def add_subscription():
+    data = {
+        'username': session['username'],
+        'artist': request.form.get('artist'),
+        'title': request.form.get('title'),
+        'path': request.form.get('path'),
+        'year': request.form.get('year')
+    }
+
+
+    response = post_request('/subscription', data)['body']
+
+
+    return render_template("subscription.html")
+
+
+@app.route('/subscription/remove', methods=['POST'])
+def remove_subscription():
+    data = {
+        'username': session['username'],
+        'song': request.form.get('song'),
+    }
+    response = delete_request('/subscription', data)
+    return load_home()
+
+
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-
-@app.route('/signup')
-def signup():
-    return render_template('register.html')
-
-
-def retrieve_body(response):
-    return json.loads(response['Payload'].read().decode('utf-8'))['body']
+    return load_home()
 
 
 def load_home():
-    response = lambda_client.invoke(
-        FunctionName='getSubscriptions',
-        InvocationType='RequestResponse',
-        Payload=json.dumps({'username': session['username']})
+    data = {'username': session['username']}
+
+    response = get_request('/music', data)['body']
+    print(response)
+
+    return render_template('home.html',
+                           data=session['username'],
+                           songs=response['Items'],
+                           )
+
+
+def get_request(resource, params):
+    response = requests.get(
+        url=url + resource,
+        headers={'Content-Type': 'application/json'},
+        params=params
     )
+    return {'status_code': response.status_code, 'body': response.json()}
 
-    response_body = retrieve_body(response)
 
-    return render_template('home.html', data=session['username'],
-                           subscriptions=response_body['subscriptions'],
-                           songs=response_body['songs'])
+def delete_request(resource, params):
+    response = requests.delete(
+        url=url + resource,
+        headers={'Content-Type': 'application/json'},
+        params=params
+    )
+    return {'status_code': response.status_code, 'body': response.json()}
+
+
+def post_request(resource, data):
+    response = requests.post(
+        url=url + resource,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps(data)
+    )
+    return {'status_code': response.status_code, 'body': response.json()}
 
 
 if __name__ == '__main__':
